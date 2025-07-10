db2bea8551ae888ac5629f54c31efb36acd52322
Brian <brian95gsus@gmail.com>
2025-07-10 11:05:38 -0400

    fix: ruta de imagenes

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,27 +2,27 @@ export const projects = [
   {
     title: 'TuReceta.app',
     description: 'Permite calcular ingredientes y costos por unidad.',
-    image: 'src/assets/tuReceta.png',
+    image: '/assets/tuReceta.png',
     url: 'https://turecetaapp.netlify.app/',
     repo:'https://github.com/Xgsusbrx',
   },
   {
     title: 'GifExpertApp',
     description: 'Buscador de GIFs con categorías dinámicas.',
-    image: 'src/assets/gifExpert.png',
+    image: '/assets/gifExpert.png',
     url: 'https://gif-expert-tarea.netlify.app/',
     repo:'https://github.com/Xgsusbrx'
   },
   {
     title: 'Calculadora de Vuelto',
     description: 'Es una calculadora pensada en los comercios en Venezuela para dar vuelto tratando con 2 monedas diferentes y sus decimales',
-    image: 'src/assets/imagencalculadora.png',
+    image: '/assets/imagencalculadora.png',
     url: 'https://calculadoradevuelto.netlify.app/',
     repo: 'https://github.com/Xgsusbrx'
   },
   {
     title:'Total Ventas del dia',
     description:'Es una app que une los totales ingresados en un negocio y hace los calculos segun el precio del dolar en el momento ',
-    image:'src/assets/imagen ventas del dia .png'
+    image:'/assets/imagen ventas del dia .png'
   }
 ];
