ef0e029645730b8f970a7c07babe54f57608f065
Brian <brian95gsus@gmail.com>
2025-07-19 08:37:56 -0400

    agrega mejoras en el formulario

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -3,21 +3,13 @@ import { useState } from "react";
 export const FormContact = () => {
   const [asunto, setAsunto] = useState("");
   const [body, setBody] = useState("");
-
-  const handleSubmit = (event) => {
-    console.log({ asunto, body });
-    event.preventDefault();
-    setAsunto("");
-    setBody("");
-  };
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form>
         <label>Asunto </label>
-        <div className="mb-2">
+        <div className="mb-2" >
           <input
-            className="border rounded-md border-gray-500"
+            className="border rounded-md border-gray-500 p-1"
             value={asunto}
             onChange={(e) => {
               setAsunto(e.target.value);
@@ -27,19 +19,19 @@ export const FormContact = () => {
         <label>Mensaje </label>
         <div className="mb-2">
           <input
-            className="border rounded-md border-gray-500"
+            className="border rounded-md border-gray-500 p-1"
             value={body}
             onChange={(e) => {
               setBody(e.target.value);
             }}
           ></input>
         </div>
-        <button
+        <a
           href={`mailto:brian95gsus@gmail.com?body=${body}&subject=${asunto}`}
           className="text-white bg-black rounded-full px-3 py-1 cursor-pointer border border-black hover:text-black hover:bg-white hover:border hover:border-black"
         >
           Enviar
-        </button>
+        </a>
       </form>
     </>
   );
